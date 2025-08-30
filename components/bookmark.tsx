83904e12d81e543854ac57aa7c3999dd4c46cf3e
Andres6936 <andres6936@gmail.com>
2025-08-30 09:13:58 -0500

    Make bookmark titles linkable with `Link` and improve code readability with consistent styling.

--- components/bookmark.tsx
+++ components/bookmark.tsx
@@ -6,7 +6,13 @@ import {Title} from "./paragraph.tsx";
 const Separator = () => {
     return (
         <View style={{alignItems: "center", justifyContent: "center", paddingVertical: "0.3cm"}}>
-            <View style={{minWidth: "70%", maxWidth: "70%", borderBottom: 0.5, borderBottomColor: "#CCC", paddingBottom: "0.1cm"}}/>
+            <View style={{
+                minWidth: "70%",
+                maxWidth: "70%",
+                borderBottom: 0.5,
+                borderBottomColor: "#CCC",
+                paddingBottom: "0.1cm"
+            }}/>
         </View>
     )
 }
@@ -24,11 +30,25 @@ const Section = (props: SectionProps) => {
                 <Title start="true" bold="true" size={9} style={{textTransform: "uppercase", opacity: 0.7}}>
                     Capítulo {props.chapter}
                 </Title>
-                <Title start="true" bold="true" size={16}>
-                    {props.title}
-                </Title>
+                <Link
+                    href={`#${getShortHash(props.title)}`}
+                    style={{
+                        color: "black",
+                        textDecoration: "none"
+                    }}
+                >
+                    <Title start="true" bold="true" size={16}>
+                        {props.title}
+                    </Title>
+                </Link>
             </View>
-            <View style={{marginLeft: "0.4cm", gap: "0.1cm", paddingLeft: "0.5cm", borderLeft: 0.5, borderLeftColor: "black"}} >
+            <View style={{
+                marginLeft: "0.4cm",
+                gap: "0.1cm",
+                paddingLeft: "0.5cm",
+                borderLeft: 0.5,
+                borderLeftColor: "black"
+            }}>
                 <Bookmark title={props.title}/>
                 {props.children}
             </View>
